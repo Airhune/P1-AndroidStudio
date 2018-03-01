4592ad1669b847f624422c18bcd2e9e45a305694
Nuria Gómez Piedrafita <ls27346@salleurl.edu>
2018-03-01 20:35:52 +0100

    P1

---
+++ Entrega1/app/src/main/java/com/example/nuria/entrega1/RecoverPwActivity.java
@@ -0,0 +1,13 @@
+package com.example.nuria.entrega1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RecoverPwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recoverpw);
+    }
+}
